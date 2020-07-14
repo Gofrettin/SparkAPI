aa79a89d576f7ba9fd995620d6249f9875f94613
Roxeez <roxeeztsu@gmail.com>
2020-07-14 17:49:16 +0200

    add missing test

--- tests/Processor/Characters/StatProcessorTest.cs
+++ tests/Processor/Characters/StatProcessorTest.cs
@@ -1,7 +1,27 @@
-﻿namespace Spark.Tests.Processor.Characters
+﻿using NFluent;
+using Spark.Packet.Characters;
+
+namespace Spark.Tests.Processor.Characters
 {
-    public class StatProcessorTest
+    public class StatProcessorTest : ProcessorTest<Stat>
     {
+        protected override Stat Packet { get; } = new Stat
+        {
+            Hp = 1000,
+            Mp = 2000,
+            MaxHp = 3000,
+            MaxMp = 4000
+        };
         
+        protected override void CheckResult()
+        {
+            Check.That(Client.Character.Hp).IsEqualTo(1000);
+            Check.That(Client.Character.Mp).IsEqualTo(2000);
+            Check.That(Client.Character.MaxHp).IsEqualTo(3000);
+            Check.That(Client.Character.MaxMp).IsEqualTo(4000);
+
+            Check.That(Client.Character.HpPercentage).IsEqualTo(33);
+            Check.That(Client.Character.MpPercentage).IsEqualTo(50);
+        }
     }
 }
